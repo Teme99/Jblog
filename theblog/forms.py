7f27cdaf332d159d3251affdf464ebfa81eae955
Teme99 <3t49631@gmail.com>
2021-11-30 20:42:15 -0800

    created the planned crud operations

---
+++ theblog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title Here...'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Write Your Blog Here...'}),
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title Here...'}),
+            'body': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Write Your Blog Here...'}),
+        }
---
+++ theblog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+# from . import views
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+
+urlpatterns = [
+    # path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/delete/<int:pk>', DeletePostView.as_view(), name="delete_post"),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('article/<int:pk>/delete', ArticleDetailView.as_view(), name="articleDetail"),
+]
